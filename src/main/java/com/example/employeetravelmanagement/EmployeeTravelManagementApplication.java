a30c4ab64a58074d8b78c344e91c2a968414c518
mohan-koduri <mohan.koduri@mindgate.in>
2024-07-24 16:44:06 +0530

    added java backend starter files

---
+++ src/main/java/com/example/employeetravelmanagement/EmployeeTravelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeetravelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTravelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTravelManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeetravelmanagement/EmployeeTravelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeetravelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTravelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
