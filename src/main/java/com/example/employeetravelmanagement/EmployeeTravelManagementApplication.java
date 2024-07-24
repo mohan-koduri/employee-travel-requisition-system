490335fae8f6224230e263f4c5a433799c54c50a
mohan-koduri <mohan.koduri@mindgate.in>
2024-07-24 17:19:05 +0530

    added application properties

--- src/main/java/com/example/employeetravelmanagement/EmployeeTravelManagementApplication.java
+++ src/main/java/com/example/employeetravelmanagement/EmployeeTravelManagementApplication.java
@@ -8,6 +8,7 @@ public class EmployeeTravelManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeTravelManagementApplication.class, args);
+		
 	}
 
 }
